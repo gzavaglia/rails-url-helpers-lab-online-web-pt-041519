f4dde109ea62f7a4f944dfb447ca6b81a7e612b9
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-26 22:28:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_column_to_students.rb
+++ db/migrate/002_add_active_column_to_students.rb
@@ -1,4 +1,4 @@
-class AddActiveToStudents < ActiveRecord::Migration
+class AddActiveColumnToStudents < ActiveRecord::Migration
   def change
     add_column :students, :active, :boolean, :default => false
   end
