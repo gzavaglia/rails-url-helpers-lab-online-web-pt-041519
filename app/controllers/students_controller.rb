4d57a3527f619e382db50f637ef8a1fa97f148f2
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-26 22:35:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,7 +16,7 @@ class StudentsController < ApplicationController
       @student.active = true
     end
     @student.save
-    redirect_to student_path(@student.to_s)
+    redirect_to student_path(@student)
     end
     
   private
